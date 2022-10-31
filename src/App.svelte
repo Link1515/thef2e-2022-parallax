645bb3327c87e8a09baa0c22df97effd5cf9b7e7
Link <h26281660@gmail.com>
2022-10-31 21:32:06 +0800

    project set up

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    123
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p class="text-red-500">
+    123
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
