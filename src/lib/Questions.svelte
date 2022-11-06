558e41184a179ce3c8207b1e160611611eee2d21
Link <h26281660@gmail.com>
2022-11-06 15:10:18 +0800

    build question (desktop)

--- src/lib/Questions.svelte
+++ src/lib/Questions.svelte
@@ -1,14 +1,22 @@
-<section class="pt-12">
-  <div class="container grid gap-10">
-    <p>你是否也有以下困擾？</p>
-    <div class="ml-auto w-9/12">
-      <img data-aos="zoom-in" src="/images/questions/01.png" alt="q-1" />
-    </div>
-    <div data-aos="zoom-in" class="w-9/12">
-      <img src="/images/questions/02.png" alt="q-2" />
-    </div>
-    <div data-aos="zoom-in" class="ml-auto w-9/12">
-      <img class="ml-auto" src="/images/questions/03.png" alt="q-3" />
+<section class="pt-12 lg:pt-40">
+  <div class="container">
+    <div class="mx-auto grid gap-10 lg:w-10/12">
+      <p class="mx-auto w-10/12 lg:text-xl">你是否也有以下困擾？</p>
+      <div
+        data-aos="zoom-in"
+        class="ml-auto w-9/12 lg:mr-auto lg:ml-0 lg:w-auto"
+      >
+        <img src="/images/questions/01.png" alt="q-1" />
+      </div>
+      <div data-aos="zoom-in" class="w-9/12 lg:ml-auto lg:w-auto">
+        <img src="/images/questions/02.png" alt="q-2" />
+      </div>
+      <div
+        data-aos="zoom-in"
+        class="ml-auto w-9/12 lg:mr-auto lg:ml-0 lg:w-auto"
+      >
+        <img src="/images/questions/03.png" alt="q-3" />
+      </div>
     </div>
   </div>
   <div class="h-48 bg-decoration-dots bg-cover bg-top bg-no-repeat" />
